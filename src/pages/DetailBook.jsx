617c774326047e620c8638f0ad388af904dabb16
arizoro <arieman69@gmail.com>
2023-11-21 14:39:33 +0700

    update 2

--- src/pages/DetailBook.jsx
+++ src/pages/DetailBook.jsx
@@ -4,7 +4,7 @@ import { ChatCenteredDots, ThumbsUp } from "@phosphor-icons/react"
 import { useState } from "react"
 
 const DetailBuku = () => {
-    const [comment, setComment] = useState()
+    const [comment, setComment] = useState(false)
     const params = useParams()
     const [like , setLike] = useState(500)
     
@@ -20,12 +20,7 @@ const DetailBuku = () => {
 
 
     const handleClick = () => {
-        return (
-            <div className="flex md:justify-start flex-wrap ">
-                <textarea name="comment" id="" placeholder="comment disini`" className=" md:w-96 h-8 w-44 " autoFocus ></textarea>
-                <button className=" bg-blue-600 w-20 border border-black rounded my-1 md:mx-2 text-white " >send</button>
-            </div>
-        )
+        setComment((open) => !open)
     }
 
     const items = data.filter((datas) => params.id == datas.id) 
@@ -39,8 +34,8 @@ const DetailBuku = () => {
                     <h1 className="text-3xl font-semibold mx-6 " >{buku.title}</h1>
                     <span className="mx-6 text-gray-500 pb-3" >Penulis : {buku.penulis}</span>
                     <p className="mx-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto quisquam pariatur reiciendis doloribus consequatur debitis eos. Error at maxime officia perferendis doloribus, odit asperiores, quidem, saepe numquam laboriosam obcaecati reiciendis blanditiis nostrum soluta veniam mollitia necessitatibus ex earum? Magnam repellendus, veritatis eum id a corporis, nulla tenetur </p>
-                    <div className=" mt-7 flex mx-6 items-center" >
-                        <button className="mx-2" onClick={()=>setComment(handleClick)} >
+                    <div className=" mt-7 flex mx-6 items-center mb-2" >
+                        <button className="mx-2" onClick={handleClick} >
                         <ChatCenteredDots size={32} />
                         </button>
                         <button onClick={handleLike} >
@@ -50,11 +45,21 @@ const DetailBuku = () => {
                             </div>
                         </button>
                     </div>
-                    <div className="mx-8 mt-2">
-                        {comment}
-                    </div>
+                    {comment ? <InputComment/> : null}
                 </div>
             </div>
+        </div>
+    )
+}
+
+const InputComment = () => {
+
+    return(
+        <div className="mx-8 mt-2">
+            <div className="flex md:justify-start flex-wrap ">
+                <textarea onChange={(e) => console.log(e.target.value)} name="comment" id="" placeholder="comment disini`" className=" md:w-96 h-8 w-44 " autoFocus ></textarea>
+                <button onClick={() => alert('comment success!')} className=" bg-blue-600 w-20 border border-black rounded my-1 md:mx-2 text-white " >send</button>
+            </div> 
         </div>
     )
 }
